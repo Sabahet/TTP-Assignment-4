c97b69d739ce772c26cf9a8b6eb3fa7f6eae13a8
Sabahet Alovic <sabaheta@yahoo.com>
2021-01-08 21:57:16 -0500

    Final Commit

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
+//Hover
 let box = document.querySelector(".dont_hover")
+
+// Button Problem
+let text = document.querySelector('.text')
+let button1 = document.querySelector(".first_button")
+let button2 = document.querySelector(".second_button")
+
+
+// Password Problem 
+let submitPass = document.querySelector(".submit")
+let addingAH1 = document.createElement('h1');
+let password = document.getElementById("#pass")
+let h = document.getElementById("h1");
+
+console.log(password)
+
+//Functions 
+submitPass.onclick = function() {
+
+        if(document.getElementById("pass").value == 12345678){
+h.insertAdjacentHTML("afterend", "Correct!");
+    } else {
+        alert("Password is incorrect")
+        
+    }
+ 
+}
+
+
 box.addEventListener("mouseover" ,event =>{
     console.log("He Hovered")
     alert("Why dont you listen to me?")
 
 })
 
-
-document.getElementById("submit").addEventListener("click", submit());
-document.getElementById("first_button").addEventListener("click",buttonOne())
-function submit(){
-    
-let password = document.getElementById("pass").value
-let addingAH1 = document.createElement('h1');
-//appendChild(addingAH1);
-console.log("IT WORKS")
-
-console.log(password)
-        if(password === 12345678){
-            alert("Password is correct!");
-    } else {
-        alert("Password is incorrect")
-        
-    }
-}
-
-function buttonOne(){
-    let place = document.querySelector("Print")
-    place.insertAdjacentHTML("I'm right</h4>");
+button1.onclick = function() {
+    text.innerText = "I'm right";
     console.log("Button one pushed")
-}
+  }
+  
+  button2.onclick = function() {
+    text.innerText = "No, I'm right";
+    console.log("Button one pushed")
+  }
+
+
 
